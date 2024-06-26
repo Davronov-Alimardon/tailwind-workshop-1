048d643b5cd83867762c8c67448ab4b92fe6b5b4
= <emailaddr>
2024-06-27 04:22:35 +0500

    added default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className="h-full min-w-full">
+      <div className="flex min-h-full bg-black items-center justify-center">
+        <Button size="lg" variant="outline">
+          <Link href="/search">Click Me</Link>
+        </Button>
+      </div>
     </div>
   );
 }
